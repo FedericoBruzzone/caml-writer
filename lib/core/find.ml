f4d05e699bd862b46deccfd08cef549d3272d599
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-09-25 00:48:25 +0200

    [FIND]: Add first version of `find` func
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lib/core/find.ml
+++ lib/core/find.ml
@@ -6,15 +6,24 @@ open Utils
 
 let editor_find () =
   editor_prompt "(ESC to cancel) Search: ";
-  if !prompt_buf.b != "" then
-    (* editor_set_status_message ("Is there a " ^ !prompt_buf.b ^ "?") *)
-    for i = 0 to get_numrows () - 1 do
-      let row = get_erow_at i in
-      let match_idx = strstr row.render !prompt_buf.b in
-      if match_idx != None then e := Some { (Option.get !e) with
-        cy = i;
-        cx = editor_row_rx_to_cx row (Option.get match_idx);
-        rowoff = get_numrows ()
-      }
-    done;
-  ab_free prompt_buf;
+  if !prompt_buf.b != "" then (
+    try
+      for i = 0 to get_numrows () - 1 do
+        let row = get_erow_at i in
+        let match_idx = strstr row.render !prompt_buf.b in
+        if match_idx != None then (
+          e :=
+            Some
+              {
+                (Option.get !e) with
+                cy = i;
+                cx = editor_row_rx_to_cx row (Option.get match_idx);
+                rowoff = get_numrows ();
+              };
+          raise Exit
+        )
+      done
+    with Exit ->
+      ();
+      ab_free prompt_buf
+  )
